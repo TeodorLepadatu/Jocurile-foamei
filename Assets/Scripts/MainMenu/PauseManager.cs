ec481b37a7d659dfb5497d5f5ba4e46a8a4d3647
bgd <trifan.bogdan09@yahoo.com>
2025-05-04 23:56:07 +0300

    Pause menu

---
+++ Assets/Scripts/MainMenu/PauseManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        // Load main menu scene if needed:
+        // SceneManager.LoadScene("MainMenu");
+    }
+}
