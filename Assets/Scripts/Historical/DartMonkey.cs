3741f2fa992eab525f16be5f0983b877d41c33d7
THEO0808 <teodor.lepadatu@yahoo.com>
2025-05-04 15:52:52 +0300

    can access second minigame

---
+++ Assets/Scripts/Historical/DartMonkey.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DartMonkey : MonoBehaviour
+{
+    public bool playerNearby = false;
+    public static bool hasCompletedMinigame = false;
+    private void Start()
+    {
+        Collider2D collider = GetComponent<Collider2D>();
+        if (collider != null)
+        {
+            collider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            playerNearby = true;
+            if(!hasCompletedMinigame)
+            {
+                SceneManager.LoadScene("TowerDefence");
+            }
+        }
+    }
+}
