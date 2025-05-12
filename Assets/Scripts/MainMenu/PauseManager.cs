2185be0441b77a655f445d13722af14aaf2db44d
Bogdan <trifan.bogdan09@yahoo.com>
2025-05-12 23:37:14 +0300

    update restart function

--- Assets/Scripts/MainMenu/PauseManager.cs
+++ Assets/Scripts/MainMenu/PauseManager.cs
@@ -25,14 +25,20 @@ public class PauseManager : MonoBehaviour
         isPaused = false;
     }
 
+    
     public void Restart()
     {
-        Scene activeScene = SceneManager.GetActiveScene();
-        
-        //SceneManager.UnloadScene(activeScene);
-        SceneManager.LoadScene("MainScene1");
         Time.timeScale = 1f;
         isPaused = false;
+        
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        if(sceneName == "TowerDefence") {
+            SceneManager.LoadScene("MainScene1");
+        }
+        else {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     void Pause()
