59c908a8f8d896eb0a6b95c4e9fb136bc7a28933
bgd <trifan.bogdan09@yahoo.com>
2025-05-17 14:42:44 +0300

    add sprites and character

---
+++ Assets/Scripts/Contemporary/Step1Controller.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Step1Controller : MonoBehaviour
+{
+    public GameObject cfe;
+    public GameObject cge;
+    public GameObject builtPC;
+    public GameObject unbuiltPC;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
