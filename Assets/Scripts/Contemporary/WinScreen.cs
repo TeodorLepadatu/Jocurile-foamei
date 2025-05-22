9558258e83f71e4c7a87ac7bed8292c38925178f
bgd <trifan.bogdan09@yahoo.com>
2025-05-22 23:01:53 +0300

    add borders, switch to the next scene after game completion

--- Assets/Scripts/Contemporary/WinScreen.cs
+++ Assets/Scripts/Contemporary/WinScreen.cs
@@ -18,7 +18,7 @@ public class WinScreen : MonoBehaviour
 
     private IEnumerator WinGame() {
         yield return new WaitForSeconds(3f);
-        SceneManager.UnloadScene("GameScene");
         SceneManager.LoadScene("ContemporaryMinigame2");
+        SceneManager.UnloadScene("GameScene");
     }
 }
