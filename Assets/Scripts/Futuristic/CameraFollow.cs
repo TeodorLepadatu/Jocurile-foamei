b25a5786894c4fd7dd701d5c1891c6278e97fc02
ROXANA-GEORGIANAASAV <asaveiroxana7@gmail.com>
2025-05-28 01:52:19 +0300

    fixed dead animation bug

--- Assets/Scripts/Futuristic/CameraFollow.cs
+++ Assets/Scripts/Futuristic/CameraFollow.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public PlayerControllerFuturistic player;
+	bool done = false;
     // Update is called once per frame
     void Update()
     {   if(player.IsRunning())
 		{
 			transform.Translate(Vector3.right * player.speed * Time.deltaTime);
+		}
+
+		if(player.IsDead() && !done)
+		{
+			transform.Translate(new Vector3(-6, 0, 0));
+			done = true;
 		}
 		
 	}
--- Assets/Scripts/Futuristic/PlayerControllerFuturistic.cs
+++ Assets/Scripts/Futuristic/PlayerControllerFuturistic.cs
@@ -95,4 +95,9 @@ public class PlayerControllerFuturistic : MonoBehaviour
 		}
 	}
 
+	public bool IsDead()
+	{
+		return anim.GetBool("isDead");
+	}
+
 }
