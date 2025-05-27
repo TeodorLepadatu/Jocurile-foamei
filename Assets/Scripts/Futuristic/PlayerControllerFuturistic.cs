23471589822dc0bd4afc50b0cd54fda6563c067b
ROXANA-GEORGIANAASAV <asaveiroxana7@gmail.com>
2025-05-27 18:14:35 +0300

    single jump

--- Assets/Scripts/Futuristic/PlayerControllerFuturistic.cs
+++ Assets/Scripts/Futuristic/PlayerControllerFuturistic.cs
@@ -6,10 +6,15 @@ public class PlayerControllerFuturistic : MonoBehaviour
 	private bool isRunning;
 	public float speed = 0;
 	public float acceleration = 0.000000000001f;
+	private Rigidbody2D rb;
+	public float jumpForce = 8f;
+	public bool isJumping = false;
+
 	void Start()
 	{
 		anim = GetComponent<Animator>();
-		
+		rb = GetComponent<Rigidbody2D>();
+
 	}
 
 	void Update()
@@ -21,7 +26,10 @@ public class PlayerControllerFuturistic : MonoBehaviour
 			speed = isRunning ? 2f : 0f;
 		}
 
-
+		if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+		{
+			Jump();
+		}
 		isRunning = anim.GetBool("isRunning");
 		if (isRunning)
 		{	
@@ -35,4 +43,19 @@ public class PlayerControllerFuturistic : MonoBehaviour
 		return isRunning;
 	}
 
+	void Jump()
+	{
+		Vector2 velocity = rb.linearVelocity;
+		velocity.y = jumpForce;
+		rb.linearVelocity = velocity;
+		isJumping = true;
+	}
+
+	void OnCollissionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Ground"))
+		{
+			isJumping = false;
+		}
+	}
 }
