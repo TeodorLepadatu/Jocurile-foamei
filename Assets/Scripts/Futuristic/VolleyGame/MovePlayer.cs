77e64a74ecd31474e22ffb5bd3a1d1bf6537a9c5
ROXANA-GEORGIANAASAV <asaveiroxana7@gmail.com>
2025-06-08 14:42:13 +0300

    player movement

---
+++ Assets/Scripts/Futuristic/VolleyGame/MovePlayer.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovePlayer : MonoBehaviour
+{
+	[Header("Movement Settings")]
+	[Tooltip("Viteza de miscare")]
+	public float moveSpeed = 5f;
+
+	private Rigidbody2D rb;
+	private Vector2 movement;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+		// dezactivam gravitatia
+		rb.gravityScale = 0f;
+
+		rb.freezeRotation = true;
+	}
+
+	void Update()
+	{
+		
+		float moveX = 0f;
+		float moveY = 0f;
+
+		if (Input.GetKey(KeyCode.RightArrow)) moveX = 1f;
+		if (Input.GetKey(KeyCode.LeftArrow)) moveX = -1f;
+		if (Input.GetKey(KeyCode.UpArrow)) moveY = 1f;
+		if (Input.GetKey(KeyCode.DownArrow)) moveY = -1f;
+
+		movement = new Vector2(moveX, moveY).normalized;
+	}
+
+	void FixedUpdate()
+	{
+		if (movement != Vector2.zero)
+		{
+			rb.linearVelocity = movement * moveSpeed;
+		}
+		else
+		{
+			rb.linearVelocity = Vector2.zero;
+		}
+	}
+}
